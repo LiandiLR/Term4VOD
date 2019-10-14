dd4a73268c0f18fbba21da8046b4f615f0227208
Jonatan Jordaan <190047@virtualwindow.co.za>
2019-10-14 13:22:58 +0200

    Update watchlist.html, watchlist.css and watchlist.js. Create navigation.css, link navigation.css. Create icons file and content, images and content

--- js/watchlist.js
+++ js/watchlist.js
@@ -3,10 +3,10 @@ $(function(){
     
     $("#Add-Class").on("click",function(){
         console.log("hell");
-        $('#AddHere').append("<div class='item col-xs-6'>asd</div>");
-        $('#AddHereMed').append("<div class='item col-xs-3'>asd</div>");
-        $('#AddHereTab').append("<div class='item col-xs-3'>asd</div>");
-        $('#AddHerePC').append("<div class='item col-xs-2'>asd</div>");
+        $('#AddHere').append("<div class='item col-xs-6'>Extra Small</div><div class='xs-spacer col-xs-3'></div>");
+        $('#AddHereMed').append("<div class='item col-xs-3'>Small/Medium</div>");
+        $('#AddHereTab').append("<div class='item col-xs-3'>Medium</div>");
+        $('#AddHerePC').append("<div class='item col-xs-2'>Large</div>");
     });
     
 });
