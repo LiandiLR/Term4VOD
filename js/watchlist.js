596229498c1d62a840316d79155db83cbb99f6f6
Jonatan Jordaan <190047@virtualwindow.co.za>
2019-10-22 09:27:52 +0200

    program API jQuery

--- js/watchlist.js
+++ js/watchlist.js
@@ -1,12 +1,33 @@
-$(function(){
+$(function() {
+    
     console.log("Hello");
+  
+    var movieslength = movies.length;
+    var randomNumberMovie = Math.floor((Math.random() * movieslength));
+    var randomMovieName = movies[randomNumberMovie];
     
-    $("#Add-Class").on("click",function(){
-        console.log("hell");
-        $('#AddHere').append("<div class='item col-xs-6'>Extra Small</div><div class='xs-spacer col-xs-3'></div>");
-        $('#AddHereMed').append("<div class='item col-xs-3'>Small/Medium</div>");
-        $('#AddHereTab').append("<div class='item col-xs-3'>Medium</div>");
-        $('#AddHerePC').append("<div class='item col-xs-2'>Large</div>");
-    });
+    var serieslength = series.length;
+    var randomNumberSeries = Math.floor((Math.random() * serieslength));
+    var randomSeriesName = series[randomNumberSeries];
     
+    console.log(movieslength);
+    console.log(randomNumberMovie);
+    console.log(randomMovieName);
+    
+    console.log(serieslength);
+    console.log(randomNumberSeries);
+    console.log(randomSeriesName);
+    
+    
+    for(i=0; i < movieslength; i++){
+        $('#AddHerePC').append("<div class='item col-xs-2'><img src="+  +"></div>");        
+    };
+    
+    var movieAPIsearch = "http://www.omdbapi.com/?apikey=c0e1ea77&plot=full&t=" + randomMovieName;
+    
+    fetch(movieAPIsearch).then(res => res.json()).then((out) => {console.log('Output: ', out);}).catch(err => console.error(err));
+    
+    var seriesAPIsearch = "http://www.omdbapi.com/?apikey=c0e1ea77&plot=full&t=" + randomSeriesName;
+    
+    fetch(seriesAPIsearch).then(res => res.json()).then((out) => {console.log('Output: ', out);}).catch(err => console.error(err));
 });
