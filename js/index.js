41607d3149bc6d313c144a9e12b34f2e074a0522
LiandiLR <190228@virtualwindow.co.za>
2019-10-29 20:21:21 +0200

    29/10/2019 Commit
    Watchlist Nav + Footer Update
    Landing Page Recommended section desktop hover state

--- js/index.js
+++ js/index.js
@@ -1,40 +1,8 @@
 $(function(){
     console.log("ready");
     
-    /*---------------- API CALL ----------------*/
-    
-    for (i = 0; i < 3; i++){
-        var movieslength = movies.length;
-        var randomNumberMovie = i + 6;
-        var randomMovieName = movies[randomNumberMovie];
-        var movieNameLength = randomMovieName.length;
-        var movieNameAPISearch = randomMovieName;
-	
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://api.themoviedb.org/3/search/movie?api_key=9b788b49ee42354dbc57b8a25b87c9df&query=" + movieNameAPISearch,
-            "method": "GET",
-            "headers": {},
-            "data": "{}"
-        }
-
-        var apiCall = settings.url;
-
-        $.getJSON(apiCall, dataCallBack);
-
-        function dataCallBack(moviesData){
-            var movieName = moviesData.results[0].original_title;
-            var moviePosterComplete = 'https://image.tmdb.org/t/p/w500' + moviesData.results[0].poster_path;
-            console.log(movieName);
-            console.log(moviePosterComplete);
-        }
-    }
-    
-	
-    
-    
-    
+    /* On Page Load, display active slide content */
+    $(".header-slide-content").fadeIn(600);
     
     /*---------------- NAVIGATION JS START ----------------*/
     
@@ -94,90 +62,66 @@ $(function(){
     });
     
     /*---------------- NAVIGATION JS END ----------------*/
+
+                
+    /*---------------- HEADER SLIDER JS START ----------------*/
+    /*	Determine Current and Next Active Slide
+    var headerSliderWidth = $(".header-slider").width();
+    var currentActiveSlideNr = $(".header-slide-active").data("slide"); /* 1 
+    var headerSliderArray = $(".header-slider").find(".header-slide"); /* 3 
+    var i = 0;
     
+    setInterval(function(){
     
-    /*---------------- HEADER JS START ----------------*/
-    
-    /* On Page Load, display active slide content */
-    $(".header-slide-content").fadeIn(600);
-    
- setInterval(function(){
-        var headerSliderWidth = $(".header-slider").width();
+        for(i = 0; i < 6; i++){
+            if(i === 3){break;}
         
-        /*	Determine Current and Next Active Slide */
-        var currentActiveSlideNr = $(".header-slide-active").data("slide");
-        
-        
-        if(currentActiveSlideNr < 3){
+            var headerSliderWidth = $(".header-slider").width();
             var nextActiveSlideNr = currentActiveSlideNr + 1;
             var circleIndexNr = currentActiveSlideNr + 1;
-        
-            /* Set circle index */
+
+            //Set circle index 
             $(".header-circle").removeClass("header-circle-active");
             $(".header-circle[data-index='" + circleIndexNr + "']").addClass("header-circle-active");
             
             // Remove Active Class + Resize Header Slide
-            $(".header-slide[data-slide='" + currentActiveSlideNr + "']").stop().animate({width:headerSliderWidth*0.15});
             $(".header-slide").removeClass("header-slide-active");
-            
-                   
+            $(".header-slide[data-slide='" + currentActiveSlideNr + "']").stop().animate({width:headerSliderWidth*0.15});
+
             // Resize Next Active Slide + Add Active Class
             $(".header-slide[data-slide='" + nextActiveSlideNr + "']").addClass("header-slide-active");
             $(".header-slide[data-slide='" + nextActiveSlideNr + "']").stop().animate({width:headerSliderWidth*0.70});
+                
             
-            // Update h3 text
-            var newActiveSlide = $(".header-slide-active").data("slide")
-            
-           /* console.log(newActiveSlide);*/
-            if(newActiveSlide === 1){
-                $(".header-text").find("h3").text("1. Avengers: Infinity War").fadeIn(800);
-            }else if(newActiveSlide === 2){
-                $(".header-text").find("h3").text("2. Captain America: The Winter Soldier").fadeIn(800);
-            }else{
-                $(".header-text").find("h3").text("3. Inception").fadeIn(800);
-            }
-                    
-        }else if(currentActiveSlideNr < 4){
-            var previousActiveSlideNr = currentActiveSlideNr - 1;
-            var circleIndexNr = currentActiveSlideNr - 1;
+         
+    if(currentActiveSlideNr =  3){
+        // Run Previous loop
         
-            // Set circle index
-            $(".header-circle").removeClass("header-circle-active");
-            $(".header-circle[data-index='" + circleIndexNr + "']").addClass("header-circle-active");
-            
-            // Remove Active Class + Resize Header Slide
-            $(".header-slide[data-slide='" + currentActiveSlideNr + "']").stop().animate({width:headerSliderWidth*0.15});
-            $(".header-slide").removeClass("header-slide-active");
-            
-                   
-            // Resize Next Active Slide + Add Active Class
-            $(".header-slide[data-slide='" + previousActiveSlideNr + "']").addClass("header-slide-active");
-            $(".header-slide[data-slide='" + previousActiveSlideNr + "']").stop().animate({width:headerSliderWidth*0.70});
-            
-            // Update h3 text
-            var newActiveSlide = $(".header-slide-active").data("slide")
-            
-            console.log(newActiveSlide);
-            if(newActiveSlide === 1){
-                $(".header-text").find("h3").text("1. Avengers: Infinity War").fadeIn(800);
-            }else if(newActiveSlide === 2){
-                $(".header-text").find("h3").text("2. Captain America: The Winter Soldier").fadeIn(800);
-            }else{
-                $(".header-text").find("h3").text("3. Inception").fadeIn(800);
-            }
-            
-        }else{
-            // Do Nothing
-        }
-     
-     
-    },3000);
+             
+                var headerSliderWidth = $(".header-slider").width();
+                var nextActiveSlideNr = currentActiveSlideNr + 1;
+                var circleIndexNr = currentActiveSlideNr + 1;
+
+                //Set circle index 
+                $(".header-circle").removeClass("header-circle-active");
+                $(".header-circle[data-index='" + circleIndexNr + "']").addClass("header-circle-active");
+
+                // Remove Active Class + Resize Header Slide
+                $(".header-slide").removeClass("header-slide-active");
+                $(".header-slide[data-slide='" + currentActiveSlideNr + "']").stop().animate({width:headerSliderWidth*0.15});
+
+
+                // Resize Next Active Slide + Add Active Class
+                $(".header-slide[data-slide='" + nextActiveSlideNr + "']").addClass("header-slide-active");
+                $(".header-slide[data-slide='" + nextActiveSlideNr + "']").stop().animate({width:headerSliderWidth*0.70});
+                
+            },3000);
     
-    /*---------------- HEADER JS START ----------------*/
+    /*---------------- HEADER SLIDER JS END ----------------*/
     
-    /*---------------- IMAGE CAROUSEL JS START ----------------*/
-    $('#carousel').carouFredSel({
-        width: 1400,
+    /*---------------- IMAGE CAROUSEL JS START ---------------- */
+    $(".carousel").carouFredSel({
+        width: 1350,
        
 		items: 6,
 		scroll: 1,
@@ -189,10 +133,86 @@ $(function(){
 		next: '#next',
 		pagination: '#pager'
     });
-	
-			
-	
     
-    /*---------------- IMAGE CAROUSEL JS END ----------------*/
+    /*---------------- IMAGE CAROUSEL JS END ----------------*/ 
+    
+    /*---------------- POSTERS HOVER START ----------------*/ 
+    $(".image-container").hover(function(){
+        $(this).find(".poster-name").slideDown();
+        $(this).find(".poster-buttons").fadeIn(500);
+        $(this).find("img").css({filter:"grayscale(100%)", opacity:"0.5", transition:"0.5s ease"})
+    },function(){
+        $(this).find(".poster-name").slideUp();
+        $(this).find(".poster-buttons").fadeOut(400);
+        $(this).find("img").css({filter:"grayscale(0%)", opacity:"1"})
+    });
+        
+   
+    
+    /*---------------- POSTERS HOVER END ----------------*/ 
+    
+    /*---------------- RECOMMENDED POSTERS  APISTART ----------------*/ 
+    
+    // Store
+    for (i = 1; i < 4; i++){
+        var movieslength = movies.length;
+        var randomNumberMovie = i + 6;
+        var randomMovieName = movies[randomNumberMovie];
+        var movieNameLength = randomMovieName.length;
+        var movieNameAPISearch = randomMovieName;
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://api.themoviedb.org/3/search/movie?api_key=9b788b49ee42354dbc57b8a25b87c9df&query=" + movieNameAPISearch,
+            "method": "GET",
+            "headers": {},
+            "data": "{}"
+        }
+        console.log(i);
+        console.log(movieNameAPISearch);
+    }
+
+        var apiCall = settings.url;
+
+        $.getJSON(apiCall, dataCallBack);
+        
+        
+
+        function dataCallBack(moviesData){
+            var movieName = moviesData.results[0].original_title;
+            var moviePosterComplete = 'https://image.tmdb.org/t/p/w500' + moviesData.results[0].poster_path;
+        }
+    
+     // Movie Name + Poster For Loop Start 
+    for (i = 1; i < 4; i++){
+        var movieslength = movies.length;
+        var randomNumberMovie = i + 6;
+        var randomMovieName = movies[randomNumberMovie];
+        var movieNameLength = randomMovieName.length;
+        var movieNameAPISearch = randomMovieName;
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://api.themoviedb.org/3/search/movie?api_key=9b788b49ee42354dbc57b8a25b87c9df&query=" + movieNameAPISearch,
+            "method": "GET",
+            "headers": {},
+            "data": "{}"
+        }
+        console.log(i);
+        console.log(movieNameAPISearch);
+    }
+
+        var apiCall = settings.url;
+
+        $.getJSON(apiCall, dataCallBack);
+        
+        
+
+        function dataCallBack(moviesData){
+            var movieName = moviesData.results[0].original_title;
+            var moviePosterComplete = 'https://image.tmdb.org/t/p/w500' + moviesData.results[0].poster_path;
+        }
     
 });
