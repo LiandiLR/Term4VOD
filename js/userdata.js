40103646023ddd0bb549ea3c7a38e9ade6304a68
kamal-69 <190123@virtualwindow.co.za>
2019-11-01 18:25:55 +0200

    signin/up_complete

---
+++ js/userdata.js
@@ -0,0 +1,6 @@
+var threeUsers = [
+    {userName : "mike69@lmao.com", userPassword : "420dab", image : "../images/mike_1"},
+    {userName : "mike420@litlaf.com", userPassword : "69lmao", image : "../images/mike_2"},
+    {userName : "trump@impeched.com", userPassword : "covfefe", image : "../images/trump"}
+]
+    
