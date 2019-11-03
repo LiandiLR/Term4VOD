a4d6b0256fe5ca8f8ee3cf5d4f095a4d0f749802
LiandiLR <190228@virtualwindow.co.za>
2019-11-03 12:23:59 +0200

    03/11/2019 LLR Commit 1
    Header display username from local storage

--- js/userdata.js
+++ js/userdata.js
@@ -1,6 +1,6 @@
 var threeUsers = [
-    {userName : "mike69@lmao.com", userPassword : "420dab", image : "../images/mike_1"},
-    {userName : "mike420@litlaf.com", userPassword : "69lmao", image : "../images/mike_2"},
-    {userName : "trump@impeched.com", userPassword : "covfefe", image : "../images/trump"}
+    {userName : "mike69@lmao.com", userPassword : "420dab", image : "../images/mike_1.jpg"},
+    {userName : "mike420@litlaf.com", userPassword : "69lmao", image : "../images/mike_2.jpg"},
+    {userName : "trump@impeched.com", userPassword : "covfefe", image : "../images/trump.jpg"}
 ]
     
