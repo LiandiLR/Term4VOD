7e394591acfbafef03d686d1bac0cee50c2435c4
LiandiLR <190228@virtualwindow.co.za>
2019-11-04 00:11:04 +0200

    04/11/2019 LLR Commit 4
    Library Filter Done!!!!!!!!!!!!!!!!!!! =) =) =)

--- js/library.js
+++ js/library.js
@@ -125,15 +125,44 @@ $(function(){
 //        };
     
     
+    /* -------------------- DISABLE FILTER BUTTONS ----------------------*/
+    /*$("#genre-textbox").on("click",function(){
+        $("#genre-textbox").removeAttr("disabled");
+        $("#title-box").attr("disabled","disabled");
+        $("#release-year").attr("disabled","disabled");
+        $("#rating-textbox").prop("disabled",true);
+    });
     
+    $("#title-box").on("click",function(){
+        $("#title-box").removeAttr("disabled");
+        $("#genre-textbox").attr("disabled","disabled");
+        $("#release-year").attr("disabled","disabled");
+        $("#rating-textbox").prop("disabled",true);
+    });
+    
+    $("#release-year").on("click",function(){
+        $("#release-year").removeAttr("disabled");
+        $("#title-box").attr("disabled","disabled");
+        $("#genre-textbox").attr("disabled","disabled");
+        $("#rating-textbox").prop("disabled","true");
+    });
+    
+    $("#rating-textbox").on("click",function(){
+        $("#rating-textbox").prop("disabled","false");
+        $("#title-box").attr("disabled","disabled");
+        $("#genre-textbox").attr("disabled","disabled");
+        $("#release-year").attr("disabled","disabled");
+    });*/
     
     /* -------------------- ON SEARCH BUTTON CLICK ----------------------*/
-    $("#search").on("click", function() {
+    $("#search").on("click", function(){
         
         /* Store selected values in variables */
         var movieTitle = $("#title-box").val();
         var genreSelected = document.getElementById("genre-textbox").value;
         var ratingSelected = document.getElementById("rating-textbox").value; 
+        var ratingSelectedArray = ratingSelected.split('+');
+        ratingSelected = ratingSelectedArray[0];
         var releaseYear = document.getElementById("release-year").value;
         
         console.log(movieTitle);  
@@ -144,18 +173,215 @@ $(function(){
         // Remove all movie posters
         $(".movie-poster").remove();
         
-        for (var i = 0; i <movieslength; i++) {
+        // LOOP THROUGH MOVIES AND SERIES JS FILE
+        for (var i = 0; i < movieslength; i++) {
+            
             if(movies[i] === movieTitle){ // Movie Title If Statement
+               console.log("title found");
                 
-                                //$("#addPosters").append("<div class='movie-poster col-xs-6 col-sm-6 col-md-3 col-lg-2'><a href='indiv.html'><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='100%'><div class='poster-hover><div class ='poster-name'>" + movieName + "</div><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='72%'><div class ='poster-buttons'><div class='poster-button-info'><i class='fas fa-info'></i></div><div class='poster-button-play'>Play</div><div class='poster-button-list'><i class='fas fa-plus'></i></div></div></a></div>");
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://api.themoviedb.org/3/search/movie?api_key=9b788b49ee42354dbc57b8a25b87c9df&query=" + movies[i],
+                    "method": "GET",
+                    "headers": {},
+                    "data": "{}"
+			     }
                 
-               /* $("#addPosters").append("<div class='poster-lg-container-mobile col-sm-6 col-md-3 col-lg-2 poster'><img src='" + + "'><div class='library-poster1'></div></div>"); */
+                $.ajax(settings).done(function (response) {
+				    //console.log("Test ", response);
+			     });
                 
-                //$(".library-poster1").find("img").attr("src", moviePosterIndivual);
-            } else{
-               console.log("title not found");
-            };    
-        }           
+                var apiCall = settings.url;
+			
+			     $.getJSON(apiCall, dataCallBack);
+			
+			     function dataCallBack(moviesData){
+				    //console.log(moviesData);
+                     
+                    var movieName = moviesData.results[0].original_title;
+                    var movieGenre = moviesData.results[0].genre_ids;
+                    var movieRatingArray = moviesData.results[0].vote_average;
+                    var movieReleaseYear = moviesData.results[0].release_date;
+                    var movieReleaseYearArray = movieReleaseYear.split('-');
+                    var movieIMDBid = moviesData.results[0].id;
+                     
+                    movieReleaseYear = movieReleaseYearArray[0];
+                    var moviePoster = 'https://image.tmdb.org/t/p/original' + moviesData.results[0].poster_path;
+                     
+                     console.log(movieName);
+                     console.log(movieGenre);
+                     console.log(movieReleaseYear);
+                     console.log(movieRatingArray);
+                     console.log(moviePoster);
+                     
+                     $("#addPosters").append("<div class='movie-poster col-xs-6 col-sm-6 col-md-3 col-lg-2'><a href='indiv.html'><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='100%'><div class='poster-hover'><div class ='poster-name'>" + movieName + "</div><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='72%'><div class ='poster-buttons'><div class='poster-button-info'><i class='fas fa-info'></i></div><div class='poster-button-play'>Play</div><div class='poster-button-list'><i class='fas fa-plus'></i></div></div></a></div>");
+                 }
+                
+            }   
+    
+        
+            // DETERMINE SELECTED GENRE ID + APPEND MOVIES WITH THE SELECTED GENRE ID
+            if ((genres[i].name) == genreSelected){
+                var genreID = genres[i].id;  
+                
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://api.themoviedb.org/3/discover/movie?api_key=9b788b49ee42354dbc57b8a25b87c9df&with_genres=" + genreID,
+                    "method": "GET",
+                    "headers": {},
+                    "data": "{}"
+			     }
+                
+                $.ajax(settings).done(function (response) {
+				    //console.log("Test ", response);
+			     });
+                
+                var apiCall = settings.url;
+			
+			     $.getJSON(apiCall, dataCallBack);
+			
+			     function dataCallBack(moviesData){
+                    console.log(moviesData);
+                    
+                    // LOOP THROUGH API MOVIES
+                    for(x = 0; x < moviesData.results.length; x++){
+                        var movieName = moviesData.results[x].original_title;
+                        var movieGenre = moviesData.results[x].genre_ids;
+                        var movieRatingArray = moviesData.results[x].vote_average;
+                        var movieReleaseYear = moviesData.results[x].release_date;
+                        
+                        var movieReleaseYearArray = movieReleaseYear.split('-');
+                        var movieIMDBid = moviesData.results[x].id;
+                        
+                        movieReleaseYear = movieReleaseYearArray[0];
+                        var moviePoster = 'https://image.tmdb.org/t/p/original' + moviesData.results[x].poster_path;
+                        
+                        console.log(movieName);
+                        console.log(movieGenre);
+                        console.log(movieReleaseYear);
+                        console.log(movieRatingArray);
+                        console.log(moviePoster);
+                    
+                        $("#addPosters").append("<div class='movie-poster col-xs-6 col-sm-6 col-md-3 col-lg-2'><a href='indiv.html'><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='100%'><div class='poster-hover'><div class ='poster-name'>" + movieName + "</div><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='72%'><div class ='poster-buttons'><div class='poster-button-info'><i class='fas fa-info'></i></div><div class='poster-button-play'>Play</div><div class='poster-button-list'><i class='fas fa-plus'></i></div></div></a></div>");
+                    }
+                 } // Data callback function end
+            }; 
+            // Genre If Statement end
+            
+            
+            // APPEND MOVIES WITH THE SPECIFIED RELEASE YEAR
+            if (releaseYear !== ""){ 
+                
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://api.themoviedb.org/3/discover/movie?api_key=9b788b49ee42354dbc57b8a25b87c9df&primary_release_year=" + releaseYear,
+                    "method": "GET",
+                    "headers": {},
+                    "data": "{}"
+			     }
+                
+                $.ajax(settings).done(function (response) {
+				    //console.log("Test ", response);
+			     });
+                
+                var apiCall = settings.url;
+			
+			     $.getJSON(apiCall, dataCallBack);
+			
+			     function dataCallBack(moviesData){
+                    console.log(moviesData);
+                    
+                    // LOOP THROUGH API MOVIES
+                    for(x = 0; x < moviesData.results.length; x++){
+                        var movieName = moviesData.results[x].original_title;
+                        var movieGenre = moviesData.results[x].genre_ids;
+                        var movieRatingArray = moviesData.results[x].vote_average;
+                        var movieReleaseYear = moviesData.results[x].release_date;
+                        
+                        var movieReleaseYearArray = movieReleaseYear.split('-');
+                        var movieIMDBid = moviesData.results[x].id;
+                        
+                        movieReleaseYear = movieReleaseYearArray[0];
+                        var moviePoster = 'https://image.tmdb.org/t/p/original' + moviesData.results[x].poster_path;
+                        
+                        console.log(movieName);
+                        console.log(movieGenre);
+                        console.log(movieReleaseYear);
+                        console.log(movieRatingArray);
+                        console.log(moviePoster);
+                    
+                        $("#addPosters").append("<div class='movie-poster col-xs-6 col-sm-6 col-md-3 col-lg-2'><a href='indiv.html'><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='100%'><div class='poster-hover'><div class ='poster-name'>" + movieName + "</div><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='72%'><div class ='poster-buttons'><div class='poster-button-info'><i class='fas fa-info'></i></div><div class='poster-button-play'>Play</div><div class='poster-button-list'><i class='fas fa-plus'></i></div></div></a></div>");
+                    }
+                 } // Data callback function end*/
+            }; 
+            // Release YearS If Statement end
+            
+            
+            
+            // APPEND MOVIES WITH THE SPECIFIED RATING
+            if(ratingSelected !== "Select Rating"){ 
+                
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://api.themoviedb.org/3/discover/movie?api_key=9b788b49ee42354dbc57b8a25b87c9df&vote_average.gte=" + ratingSelected,
+                    "method": "GET",
+                    "headers": {},
+                    "data": "{}"
+			     }
+                
+                $.ajax(settings).done(function (response) {
+				    //console.log("Test ", response);
+			     });
+                
+                var apiCall = settings.url;
+			
+			     $.getJSON(apiCall, dataCallBack);
+			
+			     function dataCallBack(moviesData){
+                    console.log(moviesData);
+                    
+                    // LOOP THROUGH API MOVIES
+                    for(x = 0; x < moviesData.results.length; x++){
+                        var movieName = moviesData.results[x].original_title;
+                        var movieGenre = moviesData.results[x].genre_ids;
+                        var movieRatingArray = moviesData.results[x].vote_average;
+                        var movieReleaseYear = moviesData.results[x].release_date;
+                        
+                        var movieReleaseYearArray = movieReleaseYear.split('-');
+                        var movieIMDBid = moviesData.results[x].id;
+                        
+                        movieReleaseYear = movieReleaseYearArray[0];
+                        var moviePoster = 'https://image.tmdb.org/t/p/original' + moviesData.results[x].poster_path;
+                        
+                        console.log(movieName);
+                        console.log(movieGenre);
+                        console.log(movieReleaseYear);
+                        console.log(movieRatingArray);
+                        console.log(moviePoster);
+                    
+                        $("#addPosters").append("<div class='movie-poster col-xs-6 col-sm-6 col-md-3 col-lg-2'><a href='indiv.html'><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='100%'><div class='poster-hover'><div class ='poster-name'>" + movieName + "</div><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='72%'><div class ='poster-buttons'><div class='poster-button-info'><i class='fas fa-info'></i></div><div class='poster-button-play'>Play</div><div class='poster-button-list'><i class='fas fa-plus'></i></div></div></a></div>");
+                    }
+                 } // Data callback function end*/
+            }; 
+            // Genre If Statement end
+            
+        }
+        // Movies and Series JS File for loop end
+                
+             
+            
+            
+            
+        
+        
+        
+        
+        
+        
         
     });
     
@@ -221,7 +447,7 @@ $(function(){
 				//$("#addPosters").append("<div class='movie-poster col-xs-6 col-sm-6 col-md-3 col-lg-2'> <div class='clickable'><img class='moviePosterImgTag' data='"+ movieIMDBid +"'  src='" + moviePoster + "' ></div></div>"); 
 				//console.log(movieName, movieGenre, movieIMDBid);
 				
-				$("#addPosters").append("<div class='movie-poster col-xs-6 col-sm-6 col-md-3 col-lg-2'><a href='indiv.html'><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='100%'><div class='poster-hover><div class ='poster-name'>" + movieName + "</div><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='72%'><div class ='poster-buttons'><div class='poster-button-info'><i class='fas fa-info'></i></div><div class='poster-button-play'>Play</div><div class='poster-button-list'><i class='fas fa-plus'></i></div></div></a></div>");
+				$("#addPosters").append("<div class='movie-poster col-xs-6 col-sm-6 col-md-3 col-lg-2'><a href='indiv.html'><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='100%'><div class='poster-hover'><div class ='poster-name'>" + movieName + "</div><img class='library-poster' data='"+ movieIMDBid + "'  src='" + moviePoster + "' width='100%' height='72%'><div class ='poster-buttons'><div class='poster-button-info'><i class='fas fa-info'></i></div><div class='poster-button-play'>Play</div><div class='poster-button-list'><i class='fas fa-plus'></i></div></div></a></div>");
                 
                 /*<div class ="image-container 5poster"><img src="images/Django.jpg" width="100%" height="100%"/><div class="poster-hover 5hover"><div class ="poster-name 5name">Django Unchained</div><img src="images/Django.jpg" width="100%" height="72%"/><div class ="poster-buttons"><div class="poster-button-info"><i class="fas fa-info"></i></div><div class="poster-button-play">Play</div><div class="poster-button-list"><i class="fas fa-plus"></i></div></div></div></div>*/
 			}; 
@@ -233,12 +459,12 @@ $(function(){
 
     /*---------------- POSTERS HOVER START ----------------*/
     
-    $(".poster-hover").on("mouseenter", function(){
-        console.log("test");
-    });
+    //$(".").on("mouseenter", function(){
+      //  console.log("test");
+    //});
     
     
-    $(".image-container").hover(function(){
+    /*$(".image-container").hover(function(){
         $(this).find(".poster-name").slideDown();
         $(this).find(".poster-buttons").fadeIn(500);
         $(this).find("img").css({filter:"grayscale(100%)", opacity:"0.5", transition:"0.5s ease"})
@@ -246,7 +472,7 @@ $(function(){
         $(this).find(".poster-name").slideUp();
         $(this).find(".poster-buttons").fadeOut(400);
         $(this).find("img").css({filter:"grayscale(0%)", opacity:"1"})
-    });        
+    });   */     
    
     
     /*---------------- POSTERS HOVER END ----------------*/ 
